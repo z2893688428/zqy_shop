a18715bd13287038e569b00f8d837cf074e3f943
zhangqinyi <z28936884280@163.com>
2021-10-18 15:18:10 +0800

    完善接口

--- src/api/index.js
+++ src/api/index.js
@@ -13,7 +13,18 @@ export const reqShops = (longitude,latitude) => ajax('/shops',{longitude,latitud
 // 04.根据经纬度和关键字搜索商铺列表
 // 05.获取一次性验证码
 // 06.用户名密码登陆
+export const reqPwdLogin = (name, pwd, captcha) => ajax('/api/login_pwd', {
+  name,
+  pwd,
+  captcha
+  }, 'POST')
 // 07.发送短信验证码
+export const reqSendCode = phone => ajax('/api/sendcode', {phone})
 // 08.手机号验证码登陆
+export const reqSmsLogin = (phone, code) => ajax('/api/login_sms', {phone, code}, 'POST')
 // 09.根据会话获取用户信息
+export const reqUser = () => ajax('/api/userinfo')
 // 10.用户登出
+export const reqLogout = () => ajax('/api/logout')
+
+
